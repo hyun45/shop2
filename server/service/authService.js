c6916e8c5af42e523ee482b13b8c3fd205c14dae
45183 <hsb9743@naver.com>
2024-02-19 10:33:54 +0900

    회원 인증 수정

--- server/service/authService.js
+++ server/service/authService.js
@@ -29,7 +29,10 @@ exports.isAdminIn = async (req, res, next) => {
 
 exports.isPermissionIn = async (req, res, next) => {
     const user = await userRepository.getUserType(req.user.email);
-    if(user.userType || req.user.email){
+    console.log(req.user.userId);
+    console.log(req.body.userId);
+    console.log(req.params.userId);
+    if(user.userType || (req.user.userId == req.params.userId) || (req.body.userId == req.user.userId)){
         next();
     } else{
         res.status(403).json(response.responseFromMessage(responseText.response_text.fail, responseText.auth_message.fail_not_permission));
